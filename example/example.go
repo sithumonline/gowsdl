540c8bc0941c3b1250d9912e9aea59b3b1cdae7a
Sithum Bopitiya <sithumsandeepap@gmail.com>
2024-07-15 20:15:25 +0530

    Rename pkg

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/sithumonline/gowsdl/example/gen"
+	"github.com/sithumonline/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
